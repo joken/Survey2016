265481d1ddfec63fe15947fe220ee2654069f22a
mi-24v <miwpayou0808@gmail.com>
2016-11-03 14:43:07 +0900

    Fragmentのリスナの実装を追加

--- Survey2016/app/src/main/java/joken/ac/jp/survey2016/MainActivity.java
+++ Survey2016/app/src/main/java/joken/ac/jp/survey2016/MainActivity.java
@@ -3,11 +3,18 @@ package joken.ac.jp.survey2016;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements QuestionFragment.OnListFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+	/** Fragmentからのコールバック */
+    @Override
+    public void onListFragmentInteraction(QuestionContent.QuestionItem item) {
+        //なんかあったらかく
     }
 }
