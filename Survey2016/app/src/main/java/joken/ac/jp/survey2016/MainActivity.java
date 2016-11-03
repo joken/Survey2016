9d268f09a2d7f4d5db35e882eed14c94c445ada6
mi-24v <miwpayou0808@gmail.com>
2016-11-03 14:43:56 +0900

    toolbarのbind追加

--- Survey2016/app/src/main/java/joken/ac/jp/survey2016/MainActivity.java
+++ Survey2016/app/src/main/java/joken/ac/jp/survey2016/MainActivity.java
@@ -1,20 +1,32 @@
 package joken.ac.jp.survey2016;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity implements QuestionFragment.OnListFragmentInteractionListener{
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends AppCompatActivity implements QuestionFragment.OnListFragmentInteractionListener {
+
+	@BindView(R.id.MainToolbar)
+	Toolbar MainToolbar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ButterKnife.bind(this);
+
+		MainToolbar.setTitle(getString(R.string.app_name));
+	}
 
 
-	/** Fragmentからのコールバック */
-    @Override
-    public void onListFragmentInteraction(QuestionContent.QuestionItem item) {
-        //なんかあったらかく
-    }
+	/**
+	 * Fragmentからのコールバック
+	 */
+	@Override
+	public void onListFragmentInteraction(QuestionContent.QuestionItem item) {
+		//なんかあったらかく
+	}
 }
