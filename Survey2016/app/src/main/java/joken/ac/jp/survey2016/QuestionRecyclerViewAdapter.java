74d1d7f7b065d570e9d3a9f908864f00b52d2656
mi-24v <miwpayou0808@gmail.com>
2016-11-03 21:14:41 +0900

    Header,Footerのロジック実装

--- Survey2016/app/src/main/java/joken/ac/jp/survey2016/QuestionRecyclerViewAdapter.java
+++ Survey2016/app/src/main/java/joken/ac/jp/survey2016/QuestionRecyclerViewAdapter.java
@@ -1,6 +1,8 @@
 package joken.ac.jp.survey2016;
 
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.BootstrapEditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -46,11 +49,11 @@ public class QuestionRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVi
 		LayoutInflater inflater = LayoutInflater.from(parent.getContext());
 		View view;
 		switch (viewType){
-			case VIEW_HEADER://TODO ヘッダレイアウト作成
-				view = inflater.inflate(R.layout.fragment_question, parent, false);
+			case VIEW_HEADER:
+				view = inflater.inflate(R.layout.fragment_header, parent, false);
 				return new HeaderViewHolder(view);
-			case VIEW_FOOTER://TODO フッタレイアウト作成
-				view = inflater.inflate(R.layout.fragment_question, parent, false);
+			case VIEW_FOOTER:
+				view = inflater.inflate(R.layout.fragment_footer, parent, false);
 				return new FooterViewHolder(view);
 			default://通常セル
 				view = inflater.inflate(R.layout.fragment_question, parent, false);
@@ -62,7 +65,7 @@ public class QuestionRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVi
 	@Override
 	public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
 		if(holder instanceof HeaderViewHolder){
-			((HeaderViewHolder)holder).onBindViewHolder();
+			((HeaderViewHolder)holder).onBindViewHolder(position);
 		}
 		if(holder instanceof FooterViewHolder){
 			((FooterViewHolder)holder).onBindViewHolder();
@@ -147,23 +150,88 @@ public class QuestionRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVi
 	}
 
 	/** ヘッダのViewHolder */
-	class HeaderViewHolder extends RecyclerView.ViewHolder{//TODO 中身実装
+	class HeaderViewHolder extends RecyclerView.ViewHolder{
+		@BindView(R.id.startButton) BootstrapButton startButton;
+		@BindView(R.id.userNameText) BootstrapEditText userNameText;
+		@BindView(R.id.maleButton) BootstrapButton maleButton;
+		@BindView(R.id.femaleButton) BootstrapButton femaleButton;
 
 		public HeaderViewHolder(View itemView) {
 			super(itemView);
+			ButterKnife.bind(this, itemView);
 		}
 
-		public void onBindViewHolder(){}
+		public void onBindViewHolder(final int position){
+			userNameText.addTextChangedListener(new TextWatcher() {
+				@Override
+				public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				}
+
+				@Override
+				public void onTextChanged(CharSequence s, int start, int before, int count) {
+					if(s.length() != 0 && (maleButton.isSelected() || femaleButton.isSelected())){
+						startButton.setEnabled(true);
+					}else{
+						startButton.setEnabled(false);
+					}
+				}
+
+				@Override
+				public void afterTextChanged(Editable s) {
+				}
+			});
+
+			BootstrapButton.OnCheckedChangedListener buttonlistener = new BootstrapButton.OnCheckedChangedListener() {
+				@Override
+				public void OnCheckedChanged(BootstrapButton bootstrapButton, boolean isChecked) {
+					if(isChecked && userNameText.getText().length() != 0){
+						startButton.setEnabled(true);
+					}else{
+						startButton.setEnabled(false);
+					}
+				}
+			};
+
+			maleButton.setOnCheckedChangedListener(buttonlistener);
+			femaleButton.setOnCheckedChangedListener(buttonlistener);
+
+			startButton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					short userSex = UserAnswer.MALE;
+					if(maleButton.isSelected())userSex = UserAnswer.MALE;
+					if(femaleButton.isSelected())userSex = UserAnswer.FEMALE;
+
+					UserAnswer.THIS.setUserName(userNameText.getText().toString());
+					UserAnswer.THIS.setUserSex(userSex);
+
+					mRecyclerView.smoothScrollToPosition(position+1);
+				}
+			});
+		}
 	}
 
 	/** フッタのViewHolder */
-	class FooterViewHolder extends RecyclerView.ViewHolder{//TODO 中身実装
+	class FooterViewHolder extends RecyclerView.ViewHolder{
+		@BindView(R.id.UserID_View) TextView userIdView;
+		@BindView(R.id.endButton) BootstrapButton endButton;
 
 		public FooterViewHolder(View itemView){
 			super(itemView);
+			ButterKnife.bind(this, itemView);
 		}
 
-		public void onBindViewHolder(){}
+		public void onBindViewHolder(){
+			userIdView.setText(UserAnswer.THIS.getUserId());
+			endButton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					UserAnswer.THIS.exportUser();
+					UserAnswer.THIS.clearAnswer();
+					mRecyclerView.scrollToPosition(0);
+				}
+			});
+		}
 	}
 
 	/** スクロール・コントローラ */
