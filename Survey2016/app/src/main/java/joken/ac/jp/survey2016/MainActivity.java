e6e011f1781a8b308432469251af42ed7c2eeea0
mi-24v <miwpayou0808.gmail.com>
2016-11-04 18:34:01 +0900

    SettingsActivityにつないだ

--- Survey2016/app/src/main/java/joken/ac/jp/survey2016/MainActivity.java
+++ Survey2016/app/src/main/java/joken/ac/jp/survey2016/MainActivity.java
@@ -1,8 +1,11 @@
 package joken.ac.jp.survey2016;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,6 +15,8 @@ public class MainActivity extends AppCompatActivity implements QuestionFragment.
 	@BindView(R.id.MainToolbar)
 	Toolbar MainToolbar;
 
+	private int toolCount = 0;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,6 +24,19 @@ public class MainActivity extends AppCompatActivity implements QuestionFragment.
 		ButterKnife.bind(this);
 
 		MainToolbar.setTitle(getString(R.string.app_name));
+		MainToolbar.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				toolCount++;
+				if(toolCount >= 10){
+					toolCount = 0;
+					Toast.makeText(getApplicationContext(), "Welcome!", Toast.LENGTH_SHORT).show();
+					startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+				}else{
+					Toast.makeText(getApplicationContext(), "Count:"+toolCount, Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
 	}
 
 
